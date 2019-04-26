029b40b376e1b95b241379a936026d4dff257bcb
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2019-04-26 12:29:05 -0400

    passing mypy for qcc/

---
+++ stubs/setuptools.pyi
@@ -0,0 +1,15 @@
+from typing import Any, Dict, List
+
+def find_packages() -> List[str]: ...
+def setup(
+        name : str,
+        version : str,
+        author : str,
+        description : str,
+        long_description : str,
+        url : str,
+        packages : List[str],
+        entry_points : Dict[str, List[str]],
+        install_requires : List[str],
+        classifiers : List[str]
+) -> None: ...
