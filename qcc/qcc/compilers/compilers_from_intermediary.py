1fa82311852cf31c1dac659c60a4b0f678d5e099
Nathanael Cho <nathanael.a.cho@gmail.com>
2019-05-02 19:13:54 -0400

    Merge Quil-QASM transpilation branch

--- qcc/qcc/compilers/compilers_from_intermediary.py
+++ qcc/qcc/compilers/compilers_from_intermediary.py
@@ -1,20 +1,48 @@
 #!/usr/bin/env python3
+from pyquil import get_qc
+
+import qcc.assembly
+from qcc.compilers.direct_compilers import QASM_IBM_Compiler
+from qcc.compilers.dependencies.quil_qasm_transpiler import \
+    Quil_QASM_Transpiler
+from qcc.hardware import Rigetti
 from qcc.interfaces import Compiler
-from qcc.hardware import IBM, Rigetti
+
+# TODO: Move any functionality out that depends on the specific
+#   implementation of the intermediary language.
 
 
-# TODO: complete and implement this set of compilers
 class Intermediary_IBM_Compiler(Compiler):
     """ Compiles Intermediary Language to IBM """
 
     def compile(self, source, target_lang):
-        # Implement this
-        return IBM("TODO")
+        """ Compile from the intermediary language to QASM. """
+
+        program = source.quil.program
+        transpiler = Quil_QASM_Transpiler()
+        circuit = transpiler.transpile(program)
+        compiled_circuit = self._compile_circuit(circuit, target_lang)
+        return compiled_circuit
+
+    @staticmethod
+    def _compile_circuit(circuit, target_lang):
+        """ Compile the Qiskit circuit for the specific target hardware. """
+
+        # Not ideal, since we go to QASM then back to the circuit
+        qasm = qcc.assembly.QASM(circuit.qasm())
+        compiler = QASM_IBM_Compiler()
+        return compiler.compile(qasm, target_lang)
 
 
 class Intermediary_Rigetti_Compiler(Compiler):
     """ Compiles Intermediary Language to Rigetti """
 
-    def compile(self, source, target_lang):
-        # Implement this
-        return Rigetti("TODO")
+    # TODO: Test once we set up the Rigetti account.
+    @staticmethod
+    def compile(source, target_lang):
+        """ Compile from the intermediary language to a Quil program. """
+
+        program = source.quil.program
+        compiler = get_qc(target_lang).compiler
+        compiled_program = compiler.quil_to_native_quil(program)
+        return Rigetti(compiled_program)
--- stubs/pyquil.pyi
+++
@@ -1,7 +0,0 @@
-class Program:
-    pass
-
-class QuantumComputer:
-    pass
-
-def get_qc(name : str) -> QuantumComputer: ...
