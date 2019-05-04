17a0a4ad7e966e339cfdb7da5d88a033f9fd0b48
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2019-05-04 16:03:58 -0400

    unit test using unittest

--- test/unit_tests/compilers/compilers_to_intermediary.py
+++ test/unit_tests/compilers/compilers_to_intermediary.py
@@ -1,5 +1,7 @@
+import os
 import unittest
 
+from qcc.assembly.qasm import QASM
 from qcc.compilers import QASM_Intermediary_Compiler
 
 class CompilersToIntermediaryTests(unittest.TestCase):
@@ -13,13 +15,16 @@ class CompilersToIntermediaryTests(unittest.TestCase):
 
     def test_qasm_intermediary_compiler_compile(self):
         """
-        Just test that this method
+        Just test that compilation
         doesn't raise exceptions.
         """
-        compiler = QASM_Intermediary_Compiler()
-
-#interm = compiler.compile(qasm_program)
-#print(interm.quil)
+        my_path = os.path.abspath(os.path.dirname(__file__))
+        qasm_file_path = os.path.join(my_path, "../../resources/qasm/src_01.qasm")
+        with open(qasm_file_path, 'r') as qasm_file:
+            qasm_str = qasm_file.read()
+            qasm_prog = QASM(qasm_str)
+            compiler = QASM_Intermediary_Compiler()
+            compiler.compile(qasm_prog)
 
 if __name__ == '__main__':
     unittest.main()
