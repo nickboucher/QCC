7664239c95726a2136d2dc20079cee9308d85288
Juan Esteller <juanvicenteesteller@gmail.com>
2019-05-05 23:21:00 -0400

    Intermediary tests also have Quil

--- test/unit_tests/compilers/test_compilers_to_intermediary.py
+++ test/unit_tests/compilers/test_compilers_to_intermediary.py
@@ -4,6 +4,10 @@ import unittest
 from qcc.assembly.qasm import QASM
 from qcc.compilers import QASM_Intermediary_Compiler
 
+from qcc.assembly.quil import Quil
+from qcc.compilers import Quil_Intermediary_Compiler
+
+
 class CompilersToIntermediaryTests(unittest.TestCase):
 
     def test_qasm_intermediary_compiler_init(self):
@@ -25,3 +29,27 @@ class CompilersToIntermediaryTests(unittest.TestCase):
             qasm_prog = QASM(qasm_str)
             compiler = QASM_Intermediary_Compiler()
             compiler.compile(qasm_prog)
+
+
+    def test_quil_intermediary_compiler_init(self):
+        """
+        Just test that compiler creation
+        doesn't raise any exceptions (as above but for Quil).
+        """
+        Quil_Intermediary_Compiler()
+
+
+    def test_quil_intermediary_compiler_compile(self):
+        """
+        Just test that compilation
+        doesn't raise exceptions, as above, but for Quil.
+        """
+
+        my_path = os.path.abspath(os.path.dirname(__file__))
+        qasm_file_path = os.path.join(my_path, "../../resources/quil/src_01.quil")
+        with open(qasm_file_path, 'r') as quil_file:
+            quil_str = quil_file.read()
+            quil_prog = Quil(quil_str)
+            compiler = Quil_Intermediary_Compiler()
+            compiler.compile(quil_prog)
+
