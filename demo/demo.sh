39d6e669f78020d0cfa7bfb8d23e65c8658cb28c
Nathanael Cho <nathanael.a.cho@gmail.com>
2019-05-07 15:25:20 -0400

    Finalize demo

--- demo/demo.sh
+++ demo/demo.sh
@@ -1,7 +1,7 @@
-cat test/resources/quil/src_01.quil
-qcc -v 2 test/resources/quil/src_01.quil --target ibmq_qasm_simulator
+cat test/resources/quil/src_12.quil
+qcc -v 2 test/resources/quil/src_12.quil --target ibmq_qasm_simulator
 cat test/resources/qasm/src_01.qasm
 qcc -v 2 test/resources/qasm/src_01.qasm --target 9q-square-qvm --stats -o /tmp/output
 cat /tmp/output
-qcc -v 2 test/resources/qasm/src_01.qasm --auto-target -o /tmp/output
+qcc -v 2 test/resources/qasm/src_01.qasm --auto-target -m num_2_qubit_gates -o /tmp/output
 cat /tmp/output
