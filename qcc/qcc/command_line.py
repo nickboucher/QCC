d6345f316a404cf1178fd775027c6f3b880d8ea9
Juan Esteller <juanvicenteesteller@gmail.com>
2019-05-09 11:37:10 -0400

    add list

--- qcc/qcc/command_line.py
+++ qcc/qcc/command_line.py
@@ -15,6 +15,8 @@ def parse_cli_args(args=None, should_init=True):
     parser = argparse.ArgumentParser()
     parser.add_argument(
         'source-file',
+        const = None,
+        nargs = '?',
         type=argparse.FileType(mode='r', encoding='utf-8'))
     parser.add_argument('--target', dest='target', type=str)
     parser.add_argument('--auto-target', dest='auto-target', action='store_true')
@@ -27,6 +29,12 @@ def parse_cli_args(args=None, should_init=True):
                              "Should be one of {'num_2_qubit_gates', 'num_insts'}. " \
                              "Defaults to 'num_2_qubit_gates'.")
     parser.add_argument('--profiles', action='store_true')
+    parser.add_argument('--list',
+                        dest='list',
+                        action='store_true',
+                        help='Print statistics about program rather than source')
+
+
     parser.add_argument('--stats',
                         dest='print_stats',
                         action='store_true',
@@ -37,17 +45,21 @@ def parse_cli_args(args=None, should_init=True):
                         type=int,
                         choices=[1, 2],
                         default=1)
+
+
+
     parser.add_argument('-o', dest='output_file', type=str)
     args = vars(parser.parse_args(args))
-    if not exactly_one_true(args['target'] is not None, args['profiles'], args['auto-target']):
-        parser.error("Must choose exactly one of {--target, --auto-target, --profiles}")
+    if not exactly_one_true(args['target'] is not None, args['profiles'], args['auto-target'], \
+            args['list']):
+        parser.error("Must choose exactly one of {--target, --auto-target, --profiles, --list}")
     if args['profiles'] and args['print_stats']:
         parser.error("Cannot use --stats flag with --profiles.")
 
     hw_target = args['target']
 
     if (hw_target and hw_target.startswith("ibmq") and should_init) or \
-            args['auto-target']:
+            args['auto-target'] or args['list']:
         qcc.qcc.init_ibmq()
 
     if hw_target and hw_target not in config.hw_langs:
@@ -64,6 +76,13 @@ def main(should_init=True, input_args=None):
     if should_init:
         qcc.init()
     args = parse_cli_args(input_args, should_init)
+
+    if args['list']:
+        print("Supported languages: " + str(config.hw_langs))
+        sys.exit(0)
+
+
+
     source_lang = qcc.get_source_lang(args['source-file'])
     if "verbosity" not in args:
         config.current_verbosity = 2
